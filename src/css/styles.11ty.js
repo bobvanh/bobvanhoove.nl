f087b264b9fcd14bd395e57590e182e48125e25a
Bob van Hoove <hoovebobvan@gmail.com>
2024-03-17 21:58:55 +0100

    Fix: retrieve purecss filepaths using the purecss module

--- src/css/styles.11ty.js
+++ src/css/styles.11ty.js
@@ -1,13 +1,15 @@
 const path = require("path");
 const fs = require("fs");
 
+const pure_min_css = "pure-min.css";
+
 module.exports = class {
     async data() {
         return {
-            permalink: '/css/main.css',
+            permalink: '/css/' + pure_min_css,
             eleventyExcludeFromCollections: true,
             // TODO : This is a janky way to get to the node_modules, figure out something better
-            sourceFile: path.join(__dirname, '..', '..', 'node_modules', 'purecss', 'build', 'pure-min.css')
+            sourceFile: path.join(__dirname, '..', '..', 'node_modules', 'purecss', 'build', pure_min_css)
         };
     }
 
